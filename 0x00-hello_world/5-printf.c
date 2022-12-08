a6bb34dee15160e46252e12b3e3fd9f813fc579f
KalidAsf <khaledakibc@gmail.com>
2022-12-08 09:43:31 -0800

    cvi

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
-#!/bin/bash
-#include <stdio.h>
+nclude <stdio.h>
 
 /**
 
@@ -15,8 +14,8 @@ int main(void)
 
 {
 
-printf("with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-return (0);
+	return (0);
 
 }
